d4aca115327d90938b907ae796423f2394fdb78e
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-11 23:00:10 +0900

    文字列フォーマットの学習

---
+++ code_9.py
@@ -0,0 +1,16 @@
+x = '山田'
+y = '太郎'
+result = f'{x + y}さん'
+print(result)
+
+z = 123
+result2 = f'No: {z:010}'
+print(result2)
+
+c = 9000000
+result3 = f'金額は{c:,}円です'
+print(result3)
+
+b = 0.123
+result4 = f'{b: .5f}'
+print(result4)
