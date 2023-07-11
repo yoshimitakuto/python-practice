771496ef31442135694d5cc948ef2044e306d6c0
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-11 19:22:09 +0900

    数値演算

---
+++ code_6.py
@@ -0,0 +1,16 @@
+calc_result = 5 * 10
+print(calc_result)
+
+# 1/10 x 1/10
+calc_result2 = 10 ** -2
+print(calc_result2)
+
+"""
+計算式の優先順位
+① 括弧
+② べき乗・べき根
+③ 掛け算・割り算・割り算余り
+④ 足し算・引き算
+"""
+calc_result3 = 3 + 4 * (1 + 1 * 2) ** 2
+print(calc_result3)
