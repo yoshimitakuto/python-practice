d6e6d15c088a52a0e901475ac549b45d0ff7dad6
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-11 15:53:03 +0900

    リスト学習終了

--- code_1.py
+++ code_1.py
@@ -5,5 +5,18 @@ print(a + b)
 x = ['a', 'b', 100]
 y = ['e', 'f']
 x.append('c')
-z = x + y
+y.remove('f')
+z = x + y # もしくはx.extend(y)
 print(z)
+
+# 開始：終了インデックスあり
+t = ['a', 'b', 'c', 'd', 'e']
+print(t[1:4])
+
+# 開始インデックスなし
+u = ['a', 'b', 'c', 'd', 'e']
+print(u[:4])
+
+# 終了インデックスなし
+i = ['a', 'b', 'c', 'd', 'e']
+print(i[2:])
