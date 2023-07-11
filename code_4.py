3543da5343f2040916b6eaf67ffb45cc20f8c16e
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-11 19:00:59 +0900

    条件式学習終了

--- code_4.py
+++ code_4.py
@@ -7,13 +7,20 @@ else:
     print('0未満')
     
 
+# リストの繰り返し
 x_list = [100, 190, 2980]
 for x in x_list:
     x_yen = str(x) + "円"
     print(x_yen)
     
 
+# 辞書の繰り返し
 x_dict = {'apple': 100, 'banana': 350}
 for key, value in x_dict.items():
     text = key + 'は' + str(value) + '円です'
     print(text)
+    
+
+# 連番の整数代入
+for y in range(10):
+    print(y)
