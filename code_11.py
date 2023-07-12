7b5e35801315c794c58bdebccbfc53b07b95f3b0
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-12 10:19:24 +0900

    追記

--- code_11.py
+++ code_11.py
@@ -15,6 +15,7 @@ with open(ファイルパス, モード) as f:
 # 既存ファイルの記載内容を取得し、4行目になったら終了文字列表示
 # readlineは行の末尾まで来て値がなかった場合から文字を返す
 with open('test.text') as f:
+    # 1行ずつ読み込む
     for _ in range(4):
         s_line = f.readline()
         print(s_line)
@@ -26,6 +27,12 @@ with open('test.text') as f:
 with open('test.text') as g:
     r_line = g.readlines()
     print(r_line)
+    
+
+# 単純にファイルの内容を読み込む方法
+with open('test.text') as k:
+    o = k.read()
+    print(o)    
     
 
 # 新規ファイル作成をして書き込み（w）
@@ -58,8 +65,8 @@ with open('test4.txt', 'r+') as h:
         print(h_line)
         if h_line == 4 and 5:
             print('4行目か5行目に値があります')
-            print(h_lien[4])
-            print(h_lien[5])
+            print(h_line[4])
+            print(h_line[5])
         elif h_line == '':
             print('終了です')
             break
